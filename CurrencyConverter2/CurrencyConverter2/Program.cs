a06cf9c203b5a088787eafa8f05cc2712f80488e
Nathan Jefferis <Nate@Nates-MacBook-Pro.local>
2019-04-11 09:09:07 -0400

    first commit

---
+++ CurrencyConverter2/CurrencyConverter2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace CurrencyConverter2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            Console.WriteLine("Enter a number:");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("Enter another number:");
+            string second = Console.ReadLine();
+
+            Console.WriteLine("Enter one last number:");
+            string third = Console.ReadLine();
+
+   
+            Single f = Convert.ToSingle(first);
+            Single s = Convert.ToSingle(second);
+            Single t = Convert.ToSingle(third);
+
+
+          
+
+
+
+
+
+            float smallest;
+            if (f <= s && f <= t)
+            {
+                smallest = f;
+            }
+            else if (s <= t && s <= f)
+            {
+                smallest = s;
+            }
+            else
+            {
+                smallest = t;
+            }
+
+            float largest;
+            if (f >= s && f >= t)
+            {
+                largest = f;
+            }
+            else if (s >= t && s >= f)
+            {
+                largest = s;
+            }
+            else
+            {
+                largest = t;
+            }
+
+            Single total = (f + s + t);
+            Single avg = (total / 3);
+
+
+            //For Japan
+
+            Console.WriteLine("Japanese " + total.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+
+            //For Sweeden
+
+            Console.WriteLine("Sweedish " + total.ToString("C", CultureInfo.CreateSpecificCulture("sv-SE")));
+
+
+            // For Thailand
+
+            Console.WriteLine("Thai: " + total.ToString("C", CultureInfo.CreateSpecificCulture("th-TH")));
+           
+
+
+            //For US
+
+            Console.WriteLine("US: " + total.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))); 
+         
+
+
+
+
+
+            Console.WriteLine("The largest ammount is " + largest);
+            Console.WriteLine("The smallest ammount is " + smallest);
+            Console.WriteLine("The average of all three dollar ammounts is " + avg);
+            //Console.WriteLine("Total value is US: $" + total );
+            //Console.WriteLine("Total value is Sweedish: " + total);
+          //  Console.WriteLine("Total value is Japanese: " + total);
+           // Console.WriteLine("Total value is Thai: ฿" + total);
+        }
+
+
+
+    }
+}
---
+++ TravelSuggestions/TravelSuggestions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TravelSuggestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer1, answer2;
+
+            Console.WriteLine("Hello user, what are you in the mood for?");
+
+
+            Console.WriteLine("Here are your options: 1) Chill Times 2) Action 3) Danger 4) Good Food");
+            answer1 = Console.ReadLine();
+
+            Console.WriteLine("How many people are you bringing with you?");
+            answer2 = Console.ReadLine();
+
+            if (answer1 == "1")
+
+                Console.WriteLine("Okay, if you're in the mood for chill times then you should go Hiking");
+            else if (answer1 == "2")
+                Console.WriteLine("Okay, if you're in the mood for adventure then you should go Stock Car Racing");
+
+            else if (answer1 == "3")
+                Console.WriteLine("Okay, if you're in the mood for danger then you should go Skydiving");
+
+            else if (answer1 == "4")
+                Console.WriteLine("Okay, if you're in the mood for good food then you should go to Taco Bell");
+
+            int i = int.Parse(answer2);
+            if (i <= 0)
+            {
+                Console.WriteLine("and you should use running shoes to get there.");
+            }
+           if (i >= 0 && i <= 4)
+            {
+                Console.WriteLine("and you should travel in a sedan.");
+            }
+           if (i >= 5 && i <= 10)
+            {
+                Console.WriteLine("and you should travel in a volkswagen bus.");
+            }
+           if (i >= 11)
+            {
+                Console.WriteLine("and you should take an airplane.");
+            }
+
+            Console.WriteLine("Goodbye.");
+
+
+        }
+
+       
+
+       
+    }
+
+
+}
+
+
